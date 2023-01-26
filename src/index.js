2ed04d53a69b606ada0d27b1019b1f3b83cb02b8
rinedein <aeternam.hiberna@gmail.com>
2023-01-26 17:15:41 +0600

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
+    if (number < 0 || number > 999) {
+        return "Invalid input. Please enter a number between 0 and 999.";
+      }
+      let ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+      let tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+      let teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    
+      let output = "";
+    
+      if (number === 0) {
+        return "zero";
+      }
+      if (number >= 100) {
+        output += ones[Math.floor(number / 100)] + " hundred ";
+        number %= 100;
+      }
+    
+      if (number >= 10 && number < 20) {
+        output += teens[number - 10] + " ";
+      } else {
+        if (number >= 10) {
+          output += tens[Math.floor(number / 10)] + " ";
+          number %= 10;
+        }
+        output += ones[number];
+      }
+    
+      return output.trim();
   
 }
